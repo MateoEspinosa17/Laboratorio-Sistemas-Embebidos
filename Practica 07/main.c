c5012060610cf23b60b4b447d7c632bfe7a4cde2
Mateo Espinosa <35758670+MateoEspinosa17@users.noreply.github.com>
2021-10-30 19:07:53 -0500

    Add files via upload

---
+++ Practica 07/main.c
@@ -0,0 +1,214 @@
+#include "sam.h"
+#include "spi.h"
+#include "myprintf.h"
+
+#define SIZE_SD_CMD 0x06
+#define kCMD00 0x40
+#define kCMD08 0x48
+#define kCMD55 0x77
+#define kCMD41 0x69
+#define RXBUFSIZE 0x400
+#define LENGTH_R1 0x03
+#define LENGTH_R7 0x07
+
+const uint8_t CMD00[SIZE_SD_CMD] = {0x40, 0x00, 0x00, 0x00, 0x00, 0x95};
+const uint8_t CMD08[SIZE_SD_CMD] = {0x48, 0x00, 0x00, 0x01, 0xAA, 0x87};
+uint8_t CMD17[SIZE_SD_CMD] = {0x51, 0x00, 0x00, 0x00, 0x00, 0x01};
+uint8_t CMD172[SIZE_SD_CMD]  ={0x51, 0x00, 0x00, 0x08, 0x00, 0x01};
+const uint8_t CMD18[SIZE_SD_CMD] = {0x52, 0x00, 0x00, 0x00, 0x00, 0x01};
+const uint8_t CMD55[SIZE_SD_CMD] = {0x77, 0x00, 0x00, 0x00, 0x00, 0x65};
+const uint8_t CMD41[SIZE_SD_CMD] = {0x69, 0x40, 0x00, 0x00, 0x00, 0x77};
+
+uint8_t RxBuffer[RXBUFSIZE];
+
+void UARTInit(void);
+void initCycles(void);
+uint32_t spiXchg(const uint8_t * send_buff, uint32_t bc, uint8_t * receive_buff );
+void rcvr_datablock(const uint8_t * send_buff, uint32_t lba, uint8_t * receive_buff, uint32_t bs );
+void startSD();
+
+int main(void)
+{
+	/* Initialize the SAM system */
+	uint32_t temp = 0xFF;
+	
+	/* Initialize the SAM system */
+	SystemInit();
+	spiInit();
+	UARTInit();
+	initCycles();
+	startSD();
+	rcvr_datablock(CMD17, 0x0000, RxBuffer, 512);
+	myprintf("\nDone\n");
+}
+
+void startSD(){
+		// Esperar a tener respuesta a CMD0
+		do{
+			spiXchg( CMD00, SIZE_SD_CMD, RxBuffer ); 
+		}while(RxBuffer[1] != 1);
+		
+		//Si ya respondio, eviamos CMD08
+		myprintf("\n");
+		spiXchg( CMD08, SIZE_SD_CMD, RxBuffer );
+		//Revisamos que no sea ilegal y que el valor recibido sea el 1 0 0 1 aa, confirmamos que es modelo nuevo
+		if((RxBuffer[1] != 1) && (RxBuffer[4] != 1) && (RxBuffer[5] != 0xaa))
+			myprintf("ERROR");
+			
+		//Enviamos CMD55 y CMD41
+		do{
+			spiXchg( CMD55, SIZE_SD_CMD, RxBuffer );
+			spiXchg( CMD41, SIZE_SD_CMD, RxBuffer );
+		} while(RxBuffer[1] != 0);
+		
+		myprintf("\nSD CARD READY");
+}
+
+void UARTInit(void) {
+	/* Initialize the SAM system */
+	SystemInit();
+	/* Switch to 8MHz clock (disable prescaler) */
+	SYSCTRL->OSC8M.bit.PRESC = 0;
+	
+	/* port mux configuration*/
+	PORT->Group[0].DIR.reg |= (1 << 10);                  /* Pin 10 configured as output */
+	PORT->Group[0].PINCFG[PIN_PA11].bit.PMUXEN = 1;       /* Enabling peripheral functions */
+	PORT->Group[0].PINCFG[PIN_PA10].bit.PMUXEN = 1;       /* Enabling peripheral functions */
+	
+	/*PMUX: even = n/2, odd: (n-1)/2 */
+	PORT->Group[0].PMUX[5].reg |= 0x02;                   /* Selecting peripheral function C */
+	PORT->Group[0].PMUX[5].reg |= 0x20;                   /* Selecting peripheral function C */
+	
+	/* APBCMASK */
+	PM->APBCMASK.reg |= PM_APBCMASK_SERCOM0;			  /* SERCOM 0 enable*/
+
+	//GCLK configuration for sercom0 module: using generic clock generator 0, ID for sercom0, enable GCLK
+
+	GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID(SERCOM0_GCLK_ID_CORE) |
+	GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN(0);
+
+	
+	/* configure SERCOM0 module for UART as Standard Frame, 8 Bit size, No parity, BAUDRATE:9600*/
+
+	SERCOM0->USART.CTRLA.reg =
+	SERCOM_USART_CTRLA_DORD | SERCOM_USART_CTRLA_MODE_USART_INT_CLK |
+	SERCOM_USART_CTRLA_RXPO(3/*PAD3*/) | SERCOM_USART_CTRLA_TXPO(1/*PAD2*/);
+	
+	uint64_t br = (uint64_t)65536 * (8000000 - 16 * 9600) / 8000000;
+	
+	SERCOM0->USART.CTRLB.reg = SERCOM_USART_CTRLB_RXEN | SERCOM_USART_CTRLB_TXEN | SERCOM_USART_CTRLB_CHSIZE(0);
+
+	SERCOM0->USART.BAUD.reg = (uint16_t)br;
+
+	SERCOM0->USART.CTRLA.reg |= SERCOM_USART_CTRLA_ENABLE;
+}
+
+void initCycles(void){
+	uint32_t i;
+	REG_PORT_OUTSET0 = PORT_PA18;
+	for(i=0;i<76;i++)
+	spiSend(0xFF);
+}
+
+uint32_t spiXchg(const uint8_t * send_buff, uint32_t bc, uint8_t * receive_buff ) {
+	uint8_t temp = 0xFF;
+	uint32_t i;
+	uint8_t temp_cmd = send_buff[0];
+	
+	REG_PORT_OUTCLR0 = PORT_PA18;
+	for(i=0; i< bc; i++) {
+		temp = spiSend(*(send_buff++));
+		//myprintf(" %x", temp);
+	}
+	switch(temp_cmd) {
+		case kCMD00 :
+		for(i=0; i<LENGTH_R1; i++) {
+			temp = spiSend(0xFF);
+			*(receive_buff++) = temp;
+			myprintf(" %x", temp);
+		}
+		break;
+		case kCMD08 :
+		for(i=0; i<LENGTH_R7; i++) {
+			temp = spiSend(0xFF);
+			*(receive_buff++) = temp;
+			myprintf(" %x", temp);
+		}
+		break;
+		case kCMD41 :
+		for(i=0; i<LENGTH_R1-1; i++) {
+			temp = spiSend(0xFF);
+			*(receive_buff++) = temp;
+			myprintf(" %x", temp);
+		}
+		spiSend(0xFF);
+		break;
+		case kCMD55 :
+		for(i=0; i<LENGTH_R1; i++) {
+			temp = spiSend(0xFF);
+			*(receive_buff++) = temp;
+			myprintf(" %x", temp);
+		}
+		break;
+		default :
+		myprintf("\n Error in CMD");
+	}
+	REG_PORT_OUTSET0 = PORT_PA18;
+	return(temp);
+}
+
+void rcvr_datablock(const uint8_t * send_buff, uint32_t lba, uint8_t * receive_buff, uint32_t bs ) {
+	uint8_t temp = 0xFF;
+	uint32_t i;
+	
+	REG_PORT_OUTCLR0 = PORT_PA18;
+	myprintf("\n\n");
+
+	temp = send_buff[0];
+	temp = spiSend(temp);
+	myprintf(" %x", temp);
+	
+	temp = ((uint8_t*)&lba)[3];
+	temp = spiSend(temp);
+	myprintf(" %x", temp);
+
+	// Complete the code that is missing
+	temp = ((uint8_t*)&lba)[2];
+	temp = spiSend(temp);
+	myprintf(" %x", temp);
+
+	temp = ((uint8_t*)&lba)[1];
+	temp = spiSend(temp);
+	myprintf(" %x", temp);
+	
+	temp = ((uint8_t*)&lba)[0];
+	temp = spiSend(temp);
+	myprintf(" %x", temp);
+
+	temp = send_buff[5];
+	temp = spiSend(temp);
+	myprintf(" %x", temp);
+
+	// Reading to find the beginning of the sector
+
+	temp = spiSend(0xFF);
+	while(temp != 0xFE){
+		temp = spiSend(0xFF);
+		myprintf(" %x", temp);
+	}
+	
+	// Receiving the memory sector/block
+	
+	myprintf("\n\n");
+	for(i=0; i< bs; i++) {
+		while(SERCOM1->SPI.INTFLAG.bit.DRE == 0);
+		SERCOM1->SPI.DATA.reg = 0xFF;
+		while(SERCOM1->SPI.INTFLAG.bit.TXC == 0);
+		while(SERCOM1->SPI.INTFLAG.bit.RXC == 0);
+		temp = SERCOM1->SPI.DATA.reg;
+		*(receive_buff++) = temp;
+		myprintf(" %x", temp);
+	}
+	REG_PORT_OUTSET0 = PORT_PA18;
+	myprintf("\n\n");
+}
